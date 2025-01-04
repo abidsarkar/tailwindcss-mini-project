dc9a1fad0beb6afcc61c039e566c5d76f79ad24f
abidsarkar <abidsarkar0@gmail.com>
2025-01-04 19:07:58 +0600

    @apply in css to avoid repeated class

--- Project_2 CountryList DropDown React And Tailwind/src/components/DropDown.jsx
+++ Project_2 CountryList DropDown React And Tailwind/src/components/DropDown.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-
+import { Country, State, City } from "country-state-city";
 const DropDown = () => {
+  console.log(Country.getAllCountries());
+  console.log(State.getAllStates());
   return (
     <div className="p-5 sm:w-[600px] bg-white/10 backdrop-blur-md border border-white/20 rounded-lg shadow-lg text-center">
       <h1 className="text-white text-lg sm:text-2xl lg:text-3xl font-bold">
@@ -8,14 +10,26 @@ const DropDown = () => {
       </h1>
       <div className="mt-5">
         {/* Country Dropdown */}
-        <select className="block w-full outline-none mb-5 bg-white/10 backdrop-blur-sm border border-white/20 rounded-lg p-2 text-white shadow-md" name="" id="">
-            <option value="">Select Country</option>
+        <select
+          className="selectStyle"
+          name=""
+          id=""
+        >
+          <option value="">Select Country</option>
         </select>
-        <select className="block w-full outline-none mb-5 bg-white/10 backdrop-blur-sm border border-white/20 rounded-lg p-2 text-white shadow-md" name="" id="">
-            <option value="">Select State</option>
+        <select
+          className="selectStyle"
+          name=""
+          id=""
+        >
+          <option value="">Select State</option>
         </select>
-        <select className="block w-full outline-none mb-5 bg-white/10 backdrop-blur-sm border border-white/20 rounded-lg p-2 text-white shadow-md" name="" id="">
-            <option value="">Select City</option>
+        <select
+          className="selectStyle"
+          name=""
+          id=""
+        >
+          <option value="">Select City</option>
         </select>
       </div>
     </div>
