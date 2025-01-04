373d3b3c4f036631df1249c08342d955f8aaaa4b
abidsarkar <abidsarkar0@gmail.com>
2025-01-04 19:42:29 +0600

    country drop down added

--- Project_2 CountryList DropDown React And Tailwind/src/components/DropDown.jsx
+++ Project_2 CountryList DropDown React And Tailwind/src/components/DropDown.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Country, State, City } from "country-state-city";
 const DropDown = () => {
-  console.log(Country.getAllCountries());
-  console.log(State.getAllStates());
+  useEffect(() => {
+    // console.log(Country.getAllCountries());
+    // console.log(State.getAllStates());
+    console.log("1st render only");
+  }, []);
+
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+  const [states, setStates] = useState([]);
+
+  const [cities, setCities] = useState([]);
+
+  const allCountries = Country.getAllCountries(); // it's an Array
+  //   handleCountryChange
+  const handleCountryChange = (e) => {
+    const countryCode = e.target.value 
+    setSelectedCountry(countryCode)
+    setCities([])
+
+    const fetchedStates = State.getStatesOfCountry(countryCode)
+    setStates(fetchedStates)
+}
   return (
     <div className="p-5 sm:w-[600px] bg-white/10 backdrop-blur-md border border-white/20 rounded-lg shadow-lg text-center">
       <h1 className="text-white text-lg sm:text-2xl lg:text-3xl font-bold">
@@ -12,23 +32,25 @@ const DropDown = () => {
         {/* Country Dropdown */}
         <select
           className="selectStyle"
-          name=""
-          id=""
+          value={selectedCountry}
+          onChange={handleCountryChange}
         >
           <option value="">Select Country</option>
+
+          {allCountries.map((country) => (
+            <option
+              key={country.isoCode}
+              value={country.isoCode}
+              className="text-black"
+            >
+              {country.name}
+            </option>
+          ))}
         </select>
-        <select
-          className="selectStyle"
-          name=""
-          id=""
-        >
+        <select className="selectStyle" name="" id="">
           <option value="">Select State</option>
         </select>
-        <select
-          className="selectStyle"
-          name=""
-          id=""
-        >
+        <select className="selectStyle" name="" id="">
           <option value="">Select City</option>
         </select>
       </div>
